4753221a625de68968a2f39c7a5b2feff8465a1f
João Pedro Nery <jppereiranery@gmail.com>
2026-08-17 21:29:39 -0300

    Pratica 03 primeira parte

--- praticas/pratica03/app-atividades-aula-tarefas/App.js
+++ praticas/pratica03/app-atividades-aula-tarefas/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import {
+  rotulo_btn_cadastro_meta,
+  rotulo_input_meta,
+  rotulo_lista_metas,
+} from "./mensagens";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={{flexDirection: "row", justifyContent: "space-between", flex: 1}}>
+        <View style={{width: "65%"}}>
+          <TextInput style={styles.InputText} placeholder={rotulo_input_meta} />
+        </View>
+        <View style={{width: "30%"}}>
+          <Button title={rotulo_btn_cadastro_meta} />
+        </View>
+      </View>
+      <View style={styles.metaContainer}>
+        <Text>{rotulo_lista_metas}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    padding: 30,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
   },
+  InputText: {
+    borderColor: "#0f3ae8",
+    borderWidth: 1,
+  },
+  metaContainer: {
+    flex: 1
+  }
 });
