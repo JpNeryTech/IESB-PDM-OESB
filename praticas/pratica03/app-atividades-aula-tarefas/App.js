4c1bcdc52f6617112657200bc65427b36cc27d08
João Pedro Nery <jppereiranery@gmail.com>
2026-08-18 21:40:36 -0300

    comitando parte 2

--- praticas/pratica03/app-atividades-aula-tarefas/App.js
+++ praticas/pratica03/app-atividades-aula-tarefas/App.js
@@ -4,20 +4,46 @@ import {
   rotulo_input_meta,
   rotulo_lista_metas,
 } from "./mensagens";
+import { useState } from "react";
 
 export default function App() {
+  const [inputMetaText, setInputMetaText] = useState("");
+  const [metas, setMetas] = useState([]);
+
+  function metaInputHandler(inputText) {
+    setInputMetaText(inputText);
+  }
+
+  function adicioarMetaHandler() {
+    setMetas([...metas, inputMetaText]);
+  }
+
   return (
     <View style={styles.mainContainer}>
-      <View style={{flexDirection: "row", justifyContent: "space-between", flex: 1}}>
-        <View style={{width: "65%"}}>
-          <TextInput style={styles.InputText} placeholder={rotulo_input_meta} />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          flex: 1,
+        }}
+      >
+        <View style={{ width: "65%" }}>
+          <TextInput
+            onChangeText={metaInputHandler}
+            style={styles.InputText}
+            placeholder={rotulo_input_meta}
+          />
         </View>
-        <View style={{width: "30%"}}>
-          <Button title={rotulo_btn_cadastro_meta} />
+        <View style={{ width: "30%" }}>
+          <Button
+            onPress={adicioarMetaHandler}
+            title={rotulo_btn_cadastro_meta}
+          />
         </View>
       </View>
+
       <View style={styles.metaContainer}>
-        <Text>{rotulo_lista_metas}</Text>
+       {metas.map((meta, index)=> <Text style={styles.item} key={index}>{meta}</Text>)}
       </View>
     </View>
   );
@@ -34,6 +60,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
   metaContainer: {
-    flex: 1
+    flex: 1,
+  },
+  item: {
+    margin: 8,
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: "#ee35db",
   }
 });
